58697356392626f49b5c7ffa3fe449f6fc8ed9f0
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 17:29:19 -0300

    :sparkles: feat(basic): added arrays program

---
+++ basic/arrays.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Simple array assignment
+algarisms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+# Access array by index
+puts algarisms[5]
+
+# Create empty array
+empty_array1 = []
+empty_array2 = Array.new # using the object "Array"
+
+# Create array of words more easily
+fruits = %w{ banana apple blueberry orange mango }
