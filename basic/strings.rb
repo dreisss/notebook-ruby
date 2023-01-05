aedd065bbb6deebc78ebdeb323ca2c135e5588cb
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 17:12:30 -0300

    :sparkles: feat(basic): added strings program

---
+++ basic/strings.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# Basic string manipulation
+# Concat strings
+puts "Hello" + " World!"
+
+# String interpolation
+world = "World"
+puts "Hello #{world}!"
+
+# Literal string
+puts 'Hello World!'
