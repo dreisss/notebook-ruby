b0b8b821c821707444a76eccbc169ab1a7aa635e
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 17:16:12 -0300

    :sparkles: feat(basic/variables): added special variables usage

--- basic/variables.rb
+++ basic/variables.rb
@@ -4,4 +4,16 @@
 hello_world = "Hello World!"
 puts hello_world
 
+# Global variable assignment
+$hello_world = "Hello World!"
+puts $hello_world
+
+# Instance variable assignment
+@hello_world = "Hello World!"
+puts @hello_world
+
+# Class variable assignment
+@@hello_world = "Hello World!"
+puts @@hello_world
+
 # Convention: use snake_case to variable names
