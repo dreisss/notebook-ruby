2c1394bcb8ed8ed6cfd00c53a42518133b768659
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 17:45:54 -0300

    :sparkles: feat(basic/arrays): added access non-existent element action

--- basic/arrays.rb
+++ basic/arrays.rb
@@ -6,6 +6,9 @@ algarisms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 # Access array by index
 puts algarisms[5]
 
+# If the element was not found it will return "nil", meaning "false" as boolean
+puts algarisms[10] == nil
+
 # Create empty array
 empty_array1 = []
 empty_array2 = Array.new # using the object "Array"
