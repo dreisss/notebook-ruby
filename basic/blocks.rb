4d727f6f0a8f6d1d99d9d61b893503340ac9d783
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 19:16:02 -0300

    :sparkles: feat(basic): added blocks program

---
+++ basic/blocks.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# Blocks are just they say, a code block
+# do
+#   puts "Hello"
+# end
+
+# You can use curly brackets too
+# { puts "Hello" }
+
+# blocks do not work alone but associated with other structures
+# Associated with a method
+def call_block
+  yield
+end
+
+call_block { puts "Hello" }
+
+# Using "each" iterator over an array
+%w{ banana apple orange }.each { |fruit| puts fruit }
+
+# alternatively
+%w{ banana apple orange }.each do |fruit|
+  puts fruit
+end
+
+# using "times" iterator over a number
+4.times do
+  puts "Times Example"
+end
+
+# using "upto" iterator over a number
+1.upto 5 do |i|
+  puts i
+end
