681d4d96da6952c833d2f45b59174178ee3e97a0
Davi Reis <reis.davi11010@gmail.com>
2023-01-05 17:56:37 -0300

    :sparkles: feat(basic): added control_structs program

---
+++ basic/control_structs.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# If statement
+if true
+  puts "True"
+elsif false
+  puts "False"
+else
+  puts "None of them"
+end
+
+# One line if statement
+puts "True" if true
+
+# While statement
+while false
+  puts "Hidden"
+end
+
+# One line while statement
+puts "Hidden" while false
+
+# Variable assignment can use these structures
