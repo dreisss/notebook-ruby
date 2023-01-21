33e3fd8fc954ce84f23734aa3dc459ef4f61cf2c
Davi Reis <reis.davi11010@gmail.com>
2023-01-21 16:34:33 -0300

    :sparkles: feat(ruby-koans): completed "about_nil" file correction

--- ruby-koans/about_nil.rb
+++ ruby-koans/about_nil.rb
@@ -1,30 +1,32 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
+# frozen_string_literal: true
 
-class AboutNil < Neo::Koan
+require File.expand_path("#{File.dirname(__FILE__)}/neo")
+
+class AboutNil < Neo::Koan # rubocop:disable Style/Documentation
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    assert_equal true, nil.is_a?(Object), 'Unlike NULL in other languages'
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
     # makes some assertions about it.
-    begin
+    begin # rubocop:disable Style/RedundantBegin
       nil.some_method_nil_doesnt_know_about
-    rescue Exception => ex
+    rescue Exception => ex # rubocop:disable Lint/RescueException, Naming/RescuedExceptionsVariableName
       # What exception has been caught?
-      assert_equal __, ex.class
+      assert_equal NoMethodError, ex.class
 
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
-      assert_match(/__/, ex.message)
+      assert_match(/nil/, ex.message)
     end
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal '', nil.to_s
+    assert_equal 'nil', nil.inspect
 
     # THINK ABOUT IT:
     #
@@ -34,5 +36,4 @@ class AboutNil < Neo::Koan
     #    obj == nil
     # Why?
   end
-
 end
