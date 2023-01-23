503bd5d188686861d023027710b0ed09f7b25eb4
Davi Reis <reis.davi11010@gmail.com>
2023-01-23 20:15:19 -0300

    :hammer: refactor(ruby-quiz/#001): refactored #001 challenge code

--- ruby-quiz/#001/solution.rb
+++ ruby-quiz/#001/solution.rb
@@ -3,24 +3,22 @@
 # Solution to challenge
 class Solution
   def clean_input(input = '')
-    processed_input = input.gsub(/[^a-zA-Z]/, '') # remove non-word chars
-
-    # fill string with 'X' to fit just five character groups
+    processed_input = input.upcase.gsub(/[^A-Z]/, '')
     processed_input << 'X' until (processed_input.length % 5).zero?
-
     processed_input
-      .upcase # uppercase the string
-      .scan(/.{5}/) # split string to five character groups
-      .join(' ') # join all character groups with space between them
   end
 
-  def string_to_numbers(cleaned_input = '')
+  def five_chars_groups(input = '')
+    clean_input(input).scan(/.{5}/).join(' ')
+  end
+
+  def letters_to_numbers(cleaned_input = '')
     result = []
+    cleaned_input.each_char { |char| result << char.ord - 64 }
+    result
+  end
 
-    cleaned_input.gsub(/\s/, '').each_char do |char|
-      result << char.ord - 64
-    end
-
-    result.join(' ')
+  def five_nums_groups(cleaned_input = '')
+    letters_to_numbers(cleaned_input).join(' ').gsub(/(\d+\s){5}/, '\0 ')
   end
 end
--- ruby-quiz/#001/tests.rb
+++ ruby-quiz/#001/tests.rb
@@ -7,23 +7,37 @@ require 'test/unit'
 
 # Test challenge solution
 class SolutionTest < Test::Unit::TestCase
-  def test_clean_input
-    solution = Solution.new
+  SOLUTION = Solution.new
 
-    assert_equal solution.clean_input('Code in Ruby, live longer!'), 'CODEI NRUBY LIVEL ONGER'
-    assert_equal solution.clean_input('Your Cipher is Working!'), 'YOURC IPHER ISWOR KINGX'
-    assert_equal solution.clean_input('Welcome to Ruby Quiz!'), 'WELCO METOR UBYQU IZXXX'
+  def test_clean_input
+    assert_equal SOLUTION.clean_input('Code in Ruby, live longer!'), 'CODEINRUBYLIVELONGER'
+    assert_equal SOLUTION.clean_input('Your Cipher is Working!'), 'YOURCIPHERISWORKINGX'
+    assert_equal SOLUTION.clean_input('Welcome to Ruby Quiz!'), 'WELCOMETORUBYQUIZXXX'
   end
 
-  def test_string_to_numbers
-    solution = Solution.new
+  def test_five_chars_groups
+    assert_equal SOLUTION.five_chars_groups('Code in Ruby, live longer!'), 'CODEI NRUBY LIVEL ONGER'
+    assert_equal SOLUTION.five_chars_groups('Your Cipher is Working!'), 'YOURC IPHER ISWOR KINGX'
+    assert_equal SOLUTION.five_chars_groups('Welcome to Ruby Quiz!'), 'WELCO METOR UBYQU IZXXX'
+  end
 
-    expected_phrase1 = '3 15 4 5 9 14 18 21 2 25 12 9 22 5 12 15 14 7 5 18'
-    expected_phrase2 = '25 15 21 18 3 9 16 8 5 18 9 19 23 15 18 11 9 14 7 24'
-    expected_phrase3 = '23 5 12 3 15 13 5 20 15 18 21 2 25 17 21 9 26 24 24 24'
+  def test_letters_to_numbers
+    expected1 = [3, 15, 4, 5, 9, 14, 18, 21, 2, 25, 12, 9, 22, 5, 12, 15, 14, 7, 5, 18]
+    expected2 = [25, 15, 21, 18, 3, 9, 16, 8, 5, 18, 9, 19, 23, 15, 18, 11, 9, 14, 7, 24]
+    expected3 = [23, 5, 12, 3, 15, 13, 5, 20, 15, 18, 21, 2, 25, 17, 21, 9, 26, 24, 24, 24]
 
-    assert_equal(solution.string_to_numbers('CODEI NRUBY LIVEL ONGER'), expected_phrase1)
-    assert_equal(solution.string_to_numbers('YOURC IPHER ISWOR KINGX'), expected_phrase2)
-    assert_equal(solution.string_to_numbers('WELCO METOR UBYQU IZXXX'), expected_phrase3)
+    assert_equal SOLUTION.letters_to_numbers('CODEINRUBYLIVELONGER'), expected1
+    assert_equal SOLUTION.letters_to_numbers('YOURCIPHERISWORKINGX'), expected2
+    assert_equal SOLUTION.letters_to_numbers('WELCOMETORUBYQUIZXXX'), expected3
+  end
+
+  def test_five_nums_groups
+    expected1 = '3 15 4 5 9  14 18 21 2 25  12 9 22 5 12  15 14 7 5 18'
+    expected2 = '25 15 21 18 3  9 16 8 5 18  9 19 23 15 18  11 9 14 7 24'
+    expected3 = '23 5 12 3 15  13 5 20 15 18  21 2 25 17 21  9 26 24 24 24'
+
+    assert_equal SOLUTION.five_nums_groups('CODEINRUBYLIVELONGER'), expected1
+    assert_equal SOLUTION.five_nums_groups('YOURCIPHERISWORKINGX'), expected2
+    assert_equal SOLUTION.five_nums_groups('WELCOMETORUBYQUIZXXX'), expected3
   end
 end
