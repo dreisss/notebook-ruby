2e2394e3de781612405c2b96a5dfb5a4df87c3eb
Davi Reis <reis.davi11010@gmail.com>
2023-01-22 22:42:16 -0300

    :sparkles: feat(ruby-koans): completed "about_sandwich_code" file correction

--- ruby-koans/about_sandwich_code.rb
+++ ruby-koans/about_sandwich_code.rb
@@ -1,35 +1,35 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
+# frozen_string_literal: true
 
-class AboutSandwichCode < Neo::Koan
+require File.expand_path("#{File.dirname(__FILE__)}/neo")
 
+class AboutSandwichCode < Neo::Koan # rubocop:disable Style/Documentation
   def count_lines(file_name)
-    file = open(file_name)
+    file = open(file_name) # rubocop:disable Security/Open
+    
     count = 0
-    while file.gets
-      count += 1
-    end
+    count += 1 while file.gets
     count
   ensure
-    file.close if file
+    file.close if file # rubocop:disable Style/SafeNavigation
   end
 
   def test_counting_lines
-    assert_equal __, count_lines("example_file.txt")
+    assert_equal 4, count_lines('example_file.txt')
   end
 
   # ------------------------------------------------------------------
 
   def find_line(file_name)
-    file = open(file_name)
-    while line = file.gets
+    file = open(file_name) # rubocop:disable Security/Open
+    while (line = file.gets)
       return line if line.match(/e/)
     end
   ensure
-    file.close if file
+    file&.close
   end
 
   def test_finding_lines
-    assert_equal __, find_line("example_file.txt")
+    assert_equal "test\n", find_line('example_file.txt')
   end
 
   # ------------------------------------------------------------------
@@ -55,10 +55,10 @@ class AboutSandwichCode < Neo::Koan
   #
 
   def file_sandwich(file_name)
-    file = open(file_name)
+    file = open(file_name) # rubocop:disable Security/Open
     yield(file)
   ensure
-    file.close if file
+    file&.close
   end
 
   # Now we write:
@@ -66,41 +66,40 @@ class AboutSandwichCode < Neo::Koan
   def count_lines2(file_name)
     file_sandwich(file_name) do |file|
       count = 0
-      while file.gets
-        count += 1
-      end
+      count += 1 while file.gets
       count
     end
   end
 
   def test_counting_lines2
-    assert_equal __, count_lines2("example_file.txt")
+    assert_equal 4, count_lines2('example_file.txt')
   end
 
   # ------------------------------------------------------------------
 
   def find_line2(file_name)
-    # Rewrite find_line using the file_sandwich library function.
+    file_sandwich(file_name) do |file|
+      while (line = file.gets)
+        return line if line.match(/e/)
+      end
+    end
   end
 
   def test_finding_lines2
-    assert_equal __, find_line2("example_file.txt")
+    assert_equal "test\n", find_line2('example_file.txt')
   end
 
   # ------------------------------------------------------------------
 
   def count_lines3(file_name)
-    open(file_name) do |file|
+    open(file_name) do |file| # rubocop:disable Security/Open
       count = 0
-      while file.gets
-        count += 1
-      end
+      count += 1 while file.gets
       count
     end
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3('example_file.txt')
   end
-
 end
