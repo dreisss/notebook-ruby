10eb79ab685486b4f9b36093d1e4cca4b532220b
Davi Reis <reis.davi11010@gmail.com>
2023-01-24 13:14:45 -0300

    :sparkles: feat(ruby-koans): completed "about_modules" file correction

--- ruby-koans/about_modules.rb
+++ ruby-koans/about_modules.rb
@@ -1,8 +1,10 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
+# frozen_string_literal: true
 
-class AboutModules < Neo::Koan
-  module Nameable
-    def set_name(new_name)
+require File.expand_path("#{File.dirname(__FILE__)}/neo")
+
+class AboutModules < Neo::Koan # rubocop:disable Style/Documentation
+  module Nameable # rubocop:disable Style/Documentation
+    def set_name(new_name) # rubocop:disable Naming/AccessorMethodName
       @name = new_name
     end
 
@@ -12,24 +14,24 @@ class AboutModules < Neo::Koan
   end
 
   def test_cant_instantiate_modules
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       Nameable.new
     end
   end
 
   # ------------------------------------------------------------------
 
-  class Dog
+  class Dog # rubocop:disable Style/Documentation
     include Nameable
 
     attr_reader :name
 
     def initialize
-      @name = "Fido"
+      @name = 'Fido'
     end
 
     def bark
-      "WOOF"
+      'WOOF'
     end
 
     def here
@@ -39,25 +41,25 @@ class AboutModules < Neo::Koan
 
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal 'WOOF', fido.bark
   end
 
   def test_module_methods_are_also_available_in_the_object
     fido = Dog.new
     assert_nothing_raised do
-      fido.set_name("Rover")
+      fido.set_name('Rover')
     end
   end
 
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal __, fido.name
-    fido.set_name("Rover")
-    assert_equal __, fido.name
+    assert_equal 'Fido', fido.name
+    fido.set_name('Rover')
+    assert_equal 'Rover', fido.name
   end
 
   def test_classes_can_override_module_methods
     fido = Dog.new
-    assert_equal __, fido.here
+    assert_equal :in_object, fido.here
   end
 end
