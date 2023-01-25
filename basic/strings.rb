a395f58ba5ab00b322ec2d2d6f6e1b20ca95ac75
Davi Reis <reis.davi11010@gmail.com>
2023-01-24 22:47:52 -0300

    :zap: chore(basic/{hello_world, strings, variables}): updated some content with ruby-koans knowledge

--- basic/strings.rb
+++ basic/strings.rb
@@ -1,13 +1,38 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
+# ruby does support all the traditional operations into strings
+# string concatenation
+'Hello' + ' World!'
 
-# Basic string manipulation
-# Concat strings
-puts 'Hello' + ' World!'
-
-# String interpolation
+# string interpolation
+# to use interpolation we must use "" instead ''
 world = 'World'
-puts "Hello #{world}!"
+"Hello #{world}!"
 
-# Literal string
-puts 'Hello World!'
+# literal strings are defined using '' instead ""
+# is a good practice use literal strings whenever possible
+'Hello World!'
+
+# access substrings
+'Hello World!'[0] # 'H'
+'Hello World!'[0, 5] # 'Hello'
+'Hello World!'[0..4] # 'Hello'
+'Hello World!'[6] # 'W'
+'Hello World!'[6, 5] # 'World'
+'Hello World!'[6..10] # 'World'
+
+# concat strings, only non-frozen literal strings can be changed
+'Hello '.concat 'World!'
+'Hello ' << 'World!'
+
+# to frozen literal strings we can use the "re-assignment" operation
+# (if possible, prefer the concat operation)
+string = 'Hello '
+string = string + 'World!'
+string += 'World!'
+
+# Some useful string methods
+'Hello World!'.equal? '' # false
+'Hello World!'.empty? # false
+'Hello World!'.upcase # 'HELLO WORLD!
+'Hello World!'.downcase # 'hello world!
+'Hello World!'.count 'l' # 3
+'Hello World!'.index 'l' # 2
